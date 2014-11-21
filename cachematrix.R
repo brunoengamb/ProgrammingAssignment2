cfc58d41936115363fa1dbf413863cec9b16afa4
brunoengamb <bruno.eng.ambiental@gmail.com>
2014-11-21 18:53:30 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a normal random quadratic  x,x matrix  
+## that matrix is cached to be used in another envorionment.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {# x is the dimention of the matrix
+  inv<<- NULL   
+  mNCols= x 
+  NRows= x 
+  myMat<<- matrix(rnorm(x*x),x,x) #set an randon quadratic matrix and cache it.
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## This function checks if the inverse of the matrix has already been calculated. 
+## If it is so, the funcion just call the cached inverse. Otherwise the funciol calculates
+## the inverse matrix.
+cacheSolve <- function() {
         ## Return a matrix that is the inverse of 'x'
+  new_myMat<- myMat
+  if(!is.null(inv)) { #cheks if the inverse matrix has already been calculated.
+    message("getting cached data.")
+    return(inv)
+  }
+  else{
+    inv<<- solve(myMat)  #calculates the inverse of the matrix 
+  }                      #if it has not been cached
+  return(inv)
 }
